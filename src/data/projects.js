8f4c37f1a75d23ee9af75aac0ed95cce0d939feb
Emil Bertlin <emilbertlin0@gmail.com>
2024-09-01 22:08:57 +0200

    init: the bones of the bear

---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+// src/data/projects.js
+export const projects = [
+    {
+        name: "project0",
+        description: "Description for project 0",
+        imgSrc: "/cm.webp",
+        link: "#",
+    },
+    {
+        name: "Langton's Ant",
+        description: "Cellular Automaton",
+        imgSrc: "/dict.webp",
+        link: "/langtons-ant",
+    },
+    {
+        name: "Project2",
+        description: "Description for project 2",
+        imgSrc: "/monet1.webp",
+        link: "#",
+    },
+
+];
