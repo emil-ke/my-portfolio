2c852e0d8bf3f0f9823ecd9c65aa5de3d969deed
Emil Bertlin <emilbertlin0@gmail.com>
2025-08-03 09:04:31 +0200

    Projects section updated: now symmetrical around the center and pulling data from projects.js properly

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,8 +2,23 @@
 export const projects = [
     {
         name: "Interactive Poetry Generator (Python)",
-        description: "A generative tool turning user-input keywords into semantically unhinged verses. Built in a day, just for fun.",
-        imgSrc: "/poetry_gen.webp",
+        description: "A generative tool which turns keywords into semantically unhinged verses. Built in a day, just for fun.",
         link: "/poetry-generator",
     },
+    {
+        name: "Drown (3D Godot Game)",
+        description: "A third-person literary survival game made in Godot. The game is still in progress. Things implemented: dialogue system, enemy AI, weapon and resource system, randomized loot spawning, etc.",
+        link: "/game",
+    },
+    {
+        name: "Gitignore (C++)",
+        description: "A CLI-tool for adding files and folders to a project's root `.gitignore`. Uses C++17.",
+        link: "/gitignore",
+    },
+    {
+        name: "Assembly Graph of Norwegian Spruce Genome (Java and Python)",
+        description: "A bioinformatics project for extracting information about an implicitly defined graph from a huge dataset. The graph algorithms are implemented in Java (variations of DFS). This extracted graph data is plotted, specifically histograms of the vertex degree and component density, using Python with matplotlib and numpy.",
+        link: "/assembly-graph",
+    },
+
 ];
