d3d502d881337c5789862bba2b06a36b203d7e47
Emil Bertlin <emilbertlin0@gmail.com>
2026-02-26 06:01:27 +0100

    style update (colors) + copy + project change

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,24 +1,23 @@
-// src/data/projects.js
 export const projects = [
     {
-        name: "Interactive Poetry Generator (Python)",
-        description: "A tool that turns keywords into semantically unhinged verses.",
-        link: "/poetry-generator",
-    },
-    {
-        name: "Drown (3D Godot Game)",
-        description: "A third-person survival game made in Godot. (In-progress.)",
+        name: "ASSAIL!",
+        description: "A third-person psychological survival mystery game.",
         link: "/game",
     },
     {
         name: "Gitignore (C++)",
-        description: "A CLI-tool for adding files and folders to a project's root `.gitignore`.",
+        description: "A CLI-tool for adding files to a Git project's root .gitignore.",
         link: "/gitignore",
     },
     {
         name: "Assembly Graph for the Norwegian Spruce Genome (Java and Python)",
         description: "A bioinformatics project for extracting information about an implicitly defined graph from a huge dataset.",
         link: "/assembly-graph",
+    },
+    {
+        name: "Interactive Poetry Generator (Python)",
+        description: "A tool that turns keywords into semantically unhinged verses.",
+        link: "/poetry-generator",
     },
 
 ];
